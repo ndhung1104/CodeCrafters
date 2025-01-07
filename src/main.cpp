2a5ad7ac63010c7562201694a6013b3952b96500
ndhung1104 <ndhung1104@gmail.com>
2025-01-07 21:46:33 +0700

    REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+bool getInput(std::string& input)
+{
+  std::cout << "$ ";
+
+  std::string input;
+  std::getline(std::cin, input);
+  if (input.size() > 0)
+    return true;
+  
+  return false;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,10 +18,8 @@ int main() {
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
   std::string input;
-  std::getline(std::cin, input);
+  while (getInput(input))
   // if (input == "invalid_command")
-  std::cout << input << ": command not found";
+    std::cout << input << ": command not found";
 }
